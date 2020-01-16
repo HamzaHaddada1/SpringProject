68aac8d04f1b301f5af52ce9826eb9808fd9fa8f
HamzaHaddada1 <hamza.haddada@gmail.com>
2020-01-16 14:37:57 +0000

    test authentification method

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,23 +1,50 @@
 package com.example.demo.service;
 
+import com.example.demo.DAO.UserRepository;
 import com.example.demo.entity.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 class UserServiceTest {
 
+    @InjectMocks
+    private UserService userService;
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     void ajouterUserTest() throws Exception{
-     UserService userService=new UserService();
         User userTest=new User(Long.valueOf(12),"hamza","123");
-       assertEquals(userService.ajouterUser(userTest),userTest);
+       when(userRepository.save(userTest)).thenReturn(userTest);
+        assertEquals(userService.ajouterUser(userTest),userTest);
     }
-   @Test
-    void hellotest() throws Exception{
-       UserService userService=new UserService();
-        assertEquals(userService.hello("hamza"),"hamza");
+    @Test
+    void authentificationtest() throws Exception{
+        User userTest=new User(Long.valueOf(12),"hamza","123");
+        User userTest1=new User();
+        userTest1=null;
+        when(userRepository.findUserByName(anyString())).thenReturn(userTest1);
+        assertTrue(userTest!=null,"user must be defined");
+        assertTrue(userTest.getPassword().equals("123")&&userTest.getName().equals("hamza"),"the user must be defined");
+
+
     }
+    @Test
+    void correctUserPass() throws Exception{
+        User userTest=new User(Long.valueOf(12),"hamza","123");
+        assertTrue(userTest.getPassword().equals("123"),"Correct userPassword");
+    }
+
 }
